24d2eabe529683ec6e90dbe3e63bbf580c05798a
solgamd <solgamd@gmail.com>
2019-05-25 22:42:59 +0200

    New refactoring to keep trying to display newArray in DOM. Not working yet...

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,52 +4,54 @@ import React, { Component } from 'react';
 import Card from './Card';
 
 class App extends Component {
-    constructor (props) {
-        super(props); 
-        this.state = {
-           films: []
-        };
+    constructor(props) {
+        super(props);
+        this.state = { 
+            films: [],
+            display: false
+        }
     }
-    componentDidMount() {
-        let title = this.state.title;
-        let description = this.state.description;
 
+    componentDidMount() {
         fetch("https://ghibliapi.herokuapp.com/films")
             .then(res => res.json())
-            .then(obj => console.log(obj))
-            
-            return this.setState({ title: title, description: description })
-        
-    };
-    // handleClick() {
-        
-    //     this.setState({ title: title, description: description }) 
-    // }
+            .then(films => this.setState({ films }))
+            .catch(error => alert(error))
+    }
+
+    renderNewArray() {
+        let newArray = this.state.films.map((film, i) => {
+            return <Card key={i} title={film.title} description={film.description} />
+        })
+        return newArray;
+    }
+
+    handleClick() {
+        return this.setState({ display: this.newArray })
+    }
+
     render() {
+
         return (
             <div className="App">
                 <header className="App-header d-flex m-2 justify-content-center">
                     <h1>Reacting to APIs</h1>
                 </header>
                 <div className="row d-flex justify-content-center">
-                    <button 
-                    className="button m-5 shadow" 
-                    onChange={e => this.componentDidMount(e)}
+                    <button
+                        className="button m-5 shadow"
+                        onClick={e => this.handleClick(e)}  //e.target.value?
                     >Load Films</button>
                 </div>
                 <section className="container">
-                <div className="row m-2">
-                    {this.state.films.map(([title, description], i) => {
-                        return (
-                            <Card key={i} title={title} description={description} />
-                        )
-                    })};
-                    
+                    <div className="row m-2">
+                        {this.state.films.display}
                     </div>
                 </section>
             </div>
-        );
+        )
     }
 }
+
 
 export default App;
